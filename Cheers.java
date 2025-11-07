53251a7634a40f8d1b4c5c0a8cbca32c582b3efa
Roni Amiel <roni.amiel@post.runi.ac.il>
2025-11-07 18:37:55 +0200

    complete

--- Cheers.java
+++ Cheers.java
@@ -7,7 +7,24 @@ Great work boaz!
 
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+public static void main(String[] args) {
+      String wordToSpell = args[0];
+      wordToSpell = wordToSpell.toUpperCase();
+      String validationLetters = "AEFHILMNORSX";
+      int timesToShout = Integer.parseInt(args [1]);
+      for (int i = 0; i < wordToSpell.length(); i++) {
+          if(validationLetters.indexOf(wordToSpell.charAt(i)) == -1){
+             System.out.println("Give me a  " + wordToSpell.charAt(i) +
+                                ": " + wordToSpell.charAt(i) + "!");
+          } else { 
+                 System.out.println("Give me an " + wordToSpell.charAt(i) + ": " + 
+                                    wordToSpell.charAt(i) + "!");
+          }
+      }  
+      System.out.println("What does that spell?");
+        for (int i = 0; i < timesToShout; i++){
+                System.out.println(wordToSpell + "!!!");
         }
-}
+  }
+ }
+
